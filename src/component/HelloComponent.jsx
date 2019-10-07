8ebe11b9ff1e9d84138527fbeec9725a8756a523
CJHydraGenZ <cahyasaga321@gmail.com>
2019-10-07 09:47:14 +0800

    push all

---
+++ src/component/HelloComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./HelloComponent.css";
+
+const HelloComponent = () => {
+  return <h1 className="text-p">Hello Cahya Selamat pagi</h1>;
+};
+
+export default HelloComponent;
